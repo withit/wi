a31ce08db6fb2cfdce6ea195f7e55d1b68a8024d
Lachlan.Sylvester <Lachlan.Sylvester@ce0115f0-7420-a745-93b4-dc4baffb057b>
2009-01-30 02:30:02 +0000

    Moved standard header and footer content into layout
    git-svn-id: http://trac.wi.com.au/svn/repo1/wi/trunk@4328 ce0115f0-7420-a745-93b4-dc4baffb057b

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   def truncate_preserving_words(text, length = 30, truncate_string = "...")
     return if text.nil?
-    l = length - truncate_string.chars.length
-    text.chars.length > length ? text[/\A.{#{l}}\w*\;?/m][/.*[\w\;]/m] + truncate_string : text
+    l = length - truncate_string.mb_chars.length
+    text.mb_chars.length > length ? text[/\A.{#{l}}\w*\;?/m][/.*[\w\;]/m] + truncate_string : text
   end
 end
