7abb2dbd80b2ca573187d87b9d31ce52162ab7d3
Lachlan.Sylvester <Lachlan.Sylvester@ce0115f0-7420-a745-93b4-dc4baffb057b>
2009-02-02 03:38:33 +0000

    Sliding effect for services
    git-svn-id: http://trac.wi.com.au/svn/repo1/wi/trunk@4369 ce0115f0-7420-a745-93b4-dc4baffb057b

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+document.observe("dom:loaded", function() {
+  // initially hide all containers for tab content
+  
+  $$('div#services p').invoke('hide');
+});
+
+Event.addBehavior({
+  'div#services a:mouseover' : function(e) {
+    Effect.BlindDown(this.up('u').next('p'),{ duration: 0.5, queue: {scope: this.name, position: 'end', limit: 1}});
+  },
+  'div#services a:mouseout' : function(e) {
+    Effect.BlindUp(this.up('u').next('p'),{ duration: 0.3, queue: {scope: this.name, position: 'end'}});
+  },
+});
