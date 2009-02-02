594decce2aae1a2298da0031e53bc59a04b87337
Lachlan.Sylvester <Lachlan.Sylvester@ce0115f0-7420-a745-93b4-dc4baffb057b>
2009-02-02 00:40:14 +0000

    Moved thoughbots shoulda and factory-girl gem config into the test env file
    git-svn-id: http://trac.wi.com.au/svn/repo1/wi/trunk@4360 ce0115f0-7420-a745-93b4-dc4baffb057b

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,7 +12,8 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
-
+config.gem "thoughtbot-shoulda", :lib => 'shoulda', :source => 'http://gems.github.com'
+config.gem "thoughtbot-factory_girl", :lib => 'factory_girl', :source => 'http://gems.github.com'
 # Disable request forgery protection in test environment
 config.action_controller.allow_forgery_protection    = false
 
