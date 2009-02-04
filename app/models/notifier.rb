238fa90d4409f558a32885a4fe3a583329bb3101
Lachlan.Sylvester <Lachlan.Sylvester@ce0115f0-7420-a745-93b4-dc4baffb057b>
2009-02-04 05:37:49 +0000

    Email to user is html format
    git-svn-id: http://trac.wi.com.au/svn/repo1/wi/trunk@4397 ce0115f0-7420-a745-93b4-dc4baffb057b

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -10,6 +10,7 @@ class Notifier < ActionMailer::Base
   def confirmation enquiry
     recipients enquiry.full_name_with_email
     from 'no-reply@wi.com.au'
+    content_type 'text/html'
     subject "Thank you for your enquiry"
     body :enquiry => enquiry
   end
