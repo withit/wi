b70be008197bf62f97a1e7964bf94fc4e5d78cde
lei.yang <lei.yang@ce0115f0-7420-a745-93b4-dc4baffb057b>
2009-09-01 07:36:38 +0000

    fixes to general wi look
    git-svn-id: http://trac.wi.com.au/svn/repo1/wi/trunk@6214 ce0115f0-7420-a745-93b4-dc4baffb057b

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_filter :authorize, :except => [:show, :index, :helped]
   before_filter :load_page, :except => :helped
   def index
-    @projects = Project.paginate(:page => params[:page], :per_page => 4, :order => 'position')
+    @projects = Project.find_all_by_current(true).paginate(:page => params[:page], :per_page => 4, :order => 'position')
   end
   
   def new
