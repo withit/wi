edcccda0f291de99f6ecae85293589b190144a72
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-12-03 15:22:51 +1100

    made links in the ipohne version of the site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   
   def index
     @page = Page.find_by_permalink('home') || Page.new
+    @projects = Project.find(:all, :conditions => 'slide_file_name is not null', :order => 'position')
   end
 
 end
