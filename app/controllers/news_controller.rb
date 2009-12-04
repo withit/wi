61c39b807281de333fc6c6589631cbeed6be502e
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-12-04 11:40:01 +1100

    refactored news months logic to use News::Month class

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -8,10 +8,10 @@ class NewsController < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        if @current_month_items.first
-          redirect_to news_item_by_month_path(:year => @current_month.year,
-                                              :month => @current_month.month,
-                                              :id => @current_month_items.first)
+        if @months.first
+          redirect_to news_item_by_month_path(:year => @month.year,
+                                              :month => @month.month,
+                                              :id => @month.news_items.first)
         else
           @current_month_items = []
         end
@@ -57,23 +57,7 @@ class NewsController < ApplicationController
   private
 
   def find_months
-    @first_month = (News.find(:first, :order => "created_at").try(:created_at) || Date.today).to_date.beginning_of_month
-    @last_month = (News.find(:last, :order => "created_at").try(:created_at) || Date.today).to_date.beginning_of_month
-    @current_month = (params[:year] and params[:month]) ?
-      Date.new(params[:year].to_i, params[:month].to_i, 1) :
-      @last_month  
-    @current_month_items = News.find(:all, :conditions => ["created_at >= ? and created_at < ?",
-                                                           @current_month,
-                                                           @current_month.end_of_month + 1])
-    
-    @window = WINDOW.map do |diff|
-      @current_month + diff.month
-    end.select do |month|
-      month >= @first_month and month <= @last_month
-    end.reverse
-    first = @window.last
-    last = @window.first
-    @window.push :next if @current_month < @last_month
-    @window.unshift :prev if @current_month > @first_month
+    @month = News::Month.new(params[:year], params[:month])
+    @months = @month.window(3)
   end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -6,4 +6,78 @@ class News < ActiveRecord::Base
   def by_month_param
     {:month => created_at.month, :year => created_at.year, :id => to_param}
   end
+  
+  class Month
+    attr_reader :year, :month
+    
+    def self.first
+      time = News.minimum(:created_at) || Time.now
+      new(time.year, time.month)
+    end
+    
+    def self.last
+      time = News.maximum(:created_at) || Time.now
+      new(time.year, time.month)
+    end
+    
+    def self.all
+    end
+    
+    def news_items
+      News.find(:all, :conditions => {:created_at => beginning_of_month..end_of_month})
+    end
+    
+    def initialize year, month
+      @month = (month || Date.today.month).to_i
+      @year = (year || Date.today.year).to_i
+    end
+    
+    def to_date
+      Date.new(@year, @month, 1)
+    end
+    
+    def succ
+      date = to_date >> 1
+      Month.new(date.year, date.month)
+    end
+    
+    def prev
+      date = to_date << 1
+      Month.new(date.year, date.month)
+    end
+    
+    # returns all months within size of this month
+    def window size
+      start_date = [to_date << size, Month.first.to_date].max
+      end_date = [to_date >> size, Month.last.to_date].min
+      start_month = Month.new(start_date.year, start_date.month)
+      end_month = Month.new(end_date.year, end_date.month)
+      
+      (start_month..end_month).to_a.reverse
+    end
+    
+    def <=> other
+      to_date <=> other.to_date
+    end
+    
+    def beginning_of_month
+      to_date.beginning_of_month
+    end
+    
+    def end_of_month
+      to_date.end_of_month
+    end
+    
+    def last?
+      self == self.class.last
+    end
+    
+    def first?
+      self == self.class.first
+    end
+    
+    def == other
+      @month == other.month && @year == other.year
+    end
+  end
 end
