32e290895b74433c40b3f6aff609653e178ba533
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-12-07 11:35:26 +1100

    fix subdomain fu config for prod

--- config/initializers/subdomain_fu.rb
+++ config/initializers/subdomain_fu.rb
@@ -1,1 +1,1 @@
-SubdomainFu.tld_sizes = {:development => 4,:test => 0, :production => 1} # set all at once (also the defaults)
+SubdomainFu.tld_sizes = {:development => 4,:test => 0, :production => 2} # set all at once (also the defaults)
