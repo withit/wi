2e9e7084f711ff96de9ae8301f25dd2984c41830
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2010-02-16 15:29:36 +1100

    some work on services and fix enlargements

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,7 @@ $(function(){
   $('body').supersleight({shim: '/images/blank.gif'});
   
   $('.website a').live('click', function(){
-    $('body').append('<div class="enlargement"><h2>' + this.innerHTML + '</h2><div><img src="' + this.href + '"<a href="#" class="close">close</a></div></div>');
+    $('body').append('<div class="enlargement"><h2>' + this.innerHTML + '</h2><div><img src="' + this.href + '"><a href="#" class="close">close</a></div></div>');
     return false;
   });
   $('.enlargement').live('click', function(){
