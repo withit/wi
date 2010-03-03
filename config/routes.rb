9ab907928b5e684afddcf767a756c50cfd3dcb26
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2010-03-04 10:54:31 +1100

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :comments
   map.resources :subscriptions
   map.enquiries 'contact-us', :controller => 'enquiries', :action => 'new', :conditions => {:method => :get}
+  map.contact_us 'contact-us', :controller => 'enquiries', :action => 'new', :conditions => {:method => :get}
   map.enquiries 'contact-us', :controller => 'enquiries', :action => 'create', :conditions => {:method => :post}
   map.resources :tags
   # The priority is based upon order of creation: first created -> highest priority.
@@ -10,55 +11,28 @@ ActionController::Routing::Routes.draw do |map|
 
   map.login 'login', :controller => 'sessions', :action => 'new'  
   map.resource :session, :only => [:create, :destroy]
-  map.resources :twitter_users
   map.resources :services
   map.projects '/projects', :controller => "pages", :action => "projects"
   map.why_choose_us '/company/why-choose-us', :controller => 'pages', :action => 'why_choose_us'
   map.root :controller  => 'home', :action => 'index'
   map.home '/', :controller => 'home', :action => 'index'  
-  map.services '/services', :controller => 'pages', :action => 'show', :id => 'services'
-  map.connect '/services.html', :controller => 'pages', :action => 'show', :id => 'services'
+
   map.subscribe '/subscribe', :controller => 'pages', :action => 'subscribe'
-  map.about '/about', :controller => 'pages', :action => 'show', :id => 'about'
-  map.connect '/about.html', :controller => 'pages', :action => 'show', :id => 'about'
 
-  map.connect '/index', :controller => 'home', :action => 'index'
-  map.connect '/index.html', :controller => 'home', :action => 'index'
-
-  map.contact_us '/contact_us', :controller => 'enquiries', :action => 'new'
-  map.connect '/contact_us.html', :controller => 'enquiries', :action => 'new'
-    
   map.connect '/company', :controller => 'pages', :action => 'company'
-  map.connect '/company.html', :controller => 'pages', :action => 'company'
 
   map.connect '/services', :controller => 'pages', :action => 'services'
-  map.connect '/services.html', :controller => 'pages', :action => 'services'
 
-  map.connect '/who_we_helped', :controller => 'projects', :action => 'helped'
-  map.connect '/who_we_helped.html', :controller => 'projects', :action => 'helped'
-
-  map.connect '/why_choose_us', :controller => 'pages', :action => 'why_choose_us'
-  map.connect '/why_choose_us.html', :controller => 'pages', :action => 'why_choose_us'
           
   map.resources :projects, :as => 'websites'
-  map.portfolio '/portfolio', :controller => 'projects', :action => 'index'  
   map.resources :news_letters, :as => 'email-campaigns', :path_prefix => 'projects'
   map.resources :banners, :path_prefix => 'projects'
                       
-  map.resources :news, :singular => 'news_item'
-  map.news_by_month 'news/:year/:month', :controller => 'news', :action => 'index'
-  map.news_item_by_month 'news/:year/:month/:id', :controller => 'news', :action => 'show'
-
   map.resource :enquiry, :only => [:create, :show]
 
-  map.resources :pages, :member => {:move_up => :put, :move_down => :put}
-
-  map.resources :assets
-  map.resources :previews
   map.resources :websites, :path_prefix => 'projects'
   map.resources :newsletters, :path_prefix => 'projects'
   map.dashboard '/dashboard', :controller => 'dashboard'
-  map.connect '/javascripts/:action.js', :controller => 'javascripts'
   map.clients '/who-weve-helped', :action => 'index', :controller => 'clients'
   map.company '/company', :action => 'index', :controller => 'pages'
   map.case_study '/case-study', :action => 'show', :controller => 'case_studies'
@@ -101,6 +75,4 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
